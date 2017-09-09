21bd3cfb67d048ed872b32810d650ffbe635c156
Alan Friedman <d.alan.friedman@gmail.com>
2017-09-09 17:56:33 -0400

    Fix warnings

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,14 +1,10 @@
 import { connect } from 'react-redux'
 import Home from '../components/Home.jsx'
-import { push, fetchBlockByName, fetchSequenceByName } from '../actions'
 
 const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = {
-  push,
-  fetchBlockByName,
-  fetchSequenceByName
 }
 
 export default connect(
