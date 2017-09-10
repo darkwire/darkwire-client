cfb927ee8c4f3b6cdd302bf19ba7957fbe1e1648
Daniel Seripap <daniel@seripap.com>
2017-09-10 10:55:35 -0400

    Stupid

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,6 @@ import styles from '../stylesheets/app.sass'
 import { Route } from 'react-router'
 import { push } from 'react-router-redux'
 import { getPublicRoutePaths, getPreventIfAuthenticatedPaths } from '../config/routes'
-import Main from '../components/Main.jsx'
 import Init from '../containers/Init'
 import Home from '../containers/Home'
 import io from '../utils/socket'
@@ -26,7 +25,6 @@ class App extends React.Component {
       <div>
         <div>
           <main>
-            <Route path="/" component={Main}/>
             <Route path="/" exact component={Init}/>
             <Route path="/:roomId" component={Home}/>
           </main>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -49,32 +49,43 @@ export default class Home extends React.Component {
 
   render() {
     return (
-      <div className='page'>
-        <div className="row">
-          <div className="col-3">
-            <h1>darkwire</h1>
+      <div className='container-fluid'>
+          <div className="row h-100">
+          <div className="col-3 right-border">
+            <div className="row bottom-border">
+              <div className="col-12">
+                <h1>darkwire</h1>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12">
+                abc
+              </div>
+            </div>
           </div>
           <div className="col-9">
-            <div className="row">
-              <div className="col-9">
-                /roomId
-              </div>
-              <div className="col-3">
-                icons
+            <div className="row h-100">
+              <div className="col-12">
+                <div className="row bottom-border">
+                  <div className="col-9">
+                    /roomId
+                  </div>
+                  <div className="col-3">
+                    icons
+                  </div>
+                </div>
+                <div className="row row-eq-height">
+                  <div className="col-12">
+                    <form onSubmit={this.handleFormSubmit.bind(this)}>
+                      <textarea className="chat" type="text" value={this.state.message} placeholder='Type here' onChange={this.handleInputChange.bind(this)}/>
+                    </form>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col-3">
-            Person
-          </div>
-          <div className="col-9">
-            <form onSubmit={this.handleFormSubmit.bind(this)}>
-              <textarea className="chat" type="text" value={this.state.message} placeholder='Type here' onChange={this.handleInputChange.bind(this)}/>
-            </form>
-          </div>
-        </div>
+
       </div>
     )
   }
