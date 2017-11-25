bc261ed515ee0891ca73bc6a446bc2625b12f9a2
Alan Friedman <d.alan.friedman@gmail.com>
2017-11-25 12:27:39 -0500

    Lint fixes

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 class Settings extends Component {
-  handleSoundToggle(e) {
+  handleSoundToggle() {
     this.props.toggleSoundEnabled(!this.props.soundIsEnabled)
   }
 
@@ -11,8 +11,8 @@ class Settings extends Component {
       <div>
         <form>
           <div className="form-check">
-            <label className="form-check-label">
-              <input onChange={this.handleSoundToggle.bind(this)} className="form-check-input" type="checkbox" checked={this.props.soundIsEnabled}/>
+            <label className="form-check-label" htmlFor="sound-control">
+              <input id="sound-control" onChange={this.handleSoundToggle.bind(this)} className="form-check-input" type="checkbox" checked={this.props.soundIsEnabled} />
               Sound
             </label>
           </div>
