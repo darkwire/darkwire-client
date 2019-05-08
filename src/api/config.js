161fe38ece1ead1054c57f9d62cf7ea5e0dae337
Alan Friedman <d.alan.friedman@gmail.com>
2019-05-08 09:23:24 -0400

    Remove API config defaults

--- src/api/config.js
+++ src/api/config.js
@@ -4,12 +4,12 @@ let port
 
 switch (process.env.NODE_ENV) {
   case 'staging':
-    host = process.env.REACT_APP_API_HOST || 'api-staging.darkwire.io'
+    host = process.env.REACT_APP_API_HOST
     protocol = process.env.REACT_APP_API_PROTOCOL || 'https'
     port = process.env.REACT_APP_API_PORT || 443
     break
   case 'production':
-    host = process.env.REACT_APP_API_HOST || 'api.darkwire.io'
+    host = process.env.REACT_APP_API_HOST
     protocol = process.env.REACT_APP_API_PROTOCOL || 'https'
     port = process.env.REACT_APP_API_PORT || 443
     break
